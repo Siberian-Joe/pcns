7accec86f41f9e25a8718ecc562cf6edc57d9ea7
Egor Volobuev <volobuev@tiltshift.xyz>
2023-12-26 12:22:00 +0700

    [Laboratory][Dev] - Added fourth lab work

---
+++ Laboratory4/ProgramA/Program.cs
@@ -0,0 +1,209 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using MPI;
+using Environment = MPI.Environment;
+
+try
+{
+    // Инициализация MPI окружения
+    using (new Environment(ref args))
+    {
+        // Получение коммуникатора MPI для всех процессов
+        var comm = Communicator.world;
+        
+        // Создание объекта для измерения времени выполнения
+        var stopwatch = Stopwatch.StartNew();
+
+        // Задаем массив URL-адресов для анализа
+        string[] urls =
+        {
+            "https://ru.wikipedia.org/wiki/Apple",
+            "https://ru.wikipedia.org/wiki/Microsoft",
+            "https://ru.wikipedia.org/wiki/Google_(компания)",
+            "https://ru.wikipedia.org/wiki/Samsung",
+            "https://ru.wikipedia.org/wiki/Nvidia",
+            "https://ru.wikipedia.org/wiki/SpaceX"
+        };
+
+        // Распределение URL-адресов по процессам
+        var urlsPerProcess = urls.Length / comm.Size;
+        var assignedUrls = DistributeUrls(urls, comm.Rank, urlsPerProcess);
+
+        // Инициализация словаря для сбора результатов
+        var allResults = new Dictionary<string, int>();
+
+        // Перебор URL-адресов, назначенных данному процессу
+        foreach (var url in assignedUrls)
+        {
+            // Получение содержимого веб-страницы
+            var content = FetchWebContent(url);
+            
+            // Извлечение годов из содержимого
+            var years = ExtractYears(content);
+            
+            // Нахождение года с наибольшим количеством событий
+            var mostEventsYear = FindYearWithMostEvents(years);
+
+            // Слияние результатов с общими результатами
+            foreach (var kvp in mostEventsYear)
+            {
+                var key = kvp.Key;
+                var value = kvp.Value;
+
+                if (allResults.TryAdd(key, value) == false)
+                    allResults[key] += value;
+            }
+        }
+
+        // Отправка результатов на корневой процесс
+        if (comm.Rank != 0)
+        {
+            comm.Send(SerializeObject(allResults), 0, 0);
+        }
+
+        // Остановка счетчика времени выполнения
+        stopwatch.Stop();
+        var processTime = stopwatch.ElapsedMilliseconds;
+
+        // Сбор и агрегация результатов на корневом процессе
+        if (comm.Rank == 0)
+        {
+            for (int i = 1; i < comm.Size; i++)
+            {
+                // Получение результатов от других процессов
+                var result = DeserializeObject<Dictionary<string, int>>(comm.Receive<byte[]>(i, 0));
+                // Агрегация результатов
+                foreach (var kvp in result)
+                {
+                    if (allResults.ContainsKey(kvp.Key))
+                        allResults[kvp.Key] += kvp.Value;
+                    else
+                        allResults[kvp.Key] = kvp.Value;
+                }
+            }
+
+            // Вывод агрегированных результатов
+            Console.WriteLine($"Общее время выполнения всех процессов: {processTime} мс");
+            // Отображение окончательных результатов
+        }
+        else
+        {
+            // Вывод времени выполнения для остальных процессов
+            Console.WriteLine($"Время выполнения процесса {comm.Rank}: {processTime} мс");
+        }
+    }
+}
+catch (Exception ex)
+{
+    // Обработка и вывод ошибки, если что-то пошло не так
+    Console.WriteLine($"Ошибка при выполнении программы: {ex.Message}");
+}
+
+// Функция DistributeUrls принимает массив URL-адресов, номер текущего процесса (rank) и количество URL-адресов,
+// которые должны быть назначены этому процессу. Она определяет, какие URL-адреса должны быть назначены текущему
+// процессу и возвращает их в виде массива строк.
+string[] DistributeUrls(string[] urls, int rank, int urlsPerProcess)
+{
+    // Общее количество URL-адресов
+    int totalUrls = urls.Length;
+    // Начальный индекс для назначения URL-адресов текущему процессу
+    int start = rank * urlsPerProcess;
+    // Конечный индекс для назначения URL-адресов текущему процессу
+    int end = (rank + 1) * urlsPerProcess;
+
+    // Проверяем, есть ли URL-адреса для распределения на текущем процессе
+    if (start >= totalUrls) return new string[0];
+
+    // Если текущий процесс последний, то он получает все оставшиеся URL-адреса
+    if (rank == Communicator.world.Size - 1)
+    {
+        end = totalUrls;
+    }
+
+    // Создаем список для хранения назначенных URL-адресов
+    List<string> assignedUrls = new List<string>();
+    // Проходим по URL-адресам и добавляем их в список назначенных
+    for (int i = start; i < end && i < totalUrls; i++)
+    {
+        assignedUrls.Add(urls[i]);
+    }
+
+    // Возвращаем назначенные URL-адреса в виде массива строк
+    return assignedUrls.ToArray();
+}
+
+// Функция FetchWebContent принимает URL-адрес и пытается получить содержимое веб-страницы с использованием HTTP-запроса.
+// Если запрос успешен, она возвращает содержимое в виде строки. В противном случае, возвращается пустая строка.
+string FetchWebContent(string url)
+{
+    try
+    {
+        using var client = new HttpClient();
+        using var response = client.GetAsync(url).Result;
+        // Если запрос успешен, считываем содержимое и возвращаем его
+        if (response.IsSuccessStatusCode)
+            return response.Content.ReadAsStringAsync().Result;
+    }
+    catch (Exception ex)
+    {
+        // В случае ошибки выводим сообщение об ошибке и возвращаем пустую строку
+        Console.WriteLine($"Ошибка при получении содержимого веб-страницы: {ex.Message}");
+    }
+
+    return string.Empty;
+}
+
+// Функция ExtractYears принимает текст веб-страницы и извлекает из него годы, используя регулярное выражение.
+// Она возвращает словарь, где ключи - это годы, а значения - количество раз, которое каждый год встречается на странице.
+Dictionary<string, int> ExtractYears(string content)
+{
+    var yearOccurrences = new Dictionary<string, int>();
+    // Регулярное выражение для поиска годов
+    var yearRegex = new Regex(@"\b(1\d{3}|20[0-1]\d|2100)\b");
+
+    foreach (Match match in yearRegex.Matches(content))
+    {
+        var year = match.Value;
+        if (yearOccurrences.TryAdd(year, 1) == false)
+        {
+            yearOccurrences[year]++;
+        }
+    }
+
+    return yearOccurrences;
+}
+
+// Функция FindYearWithMostEvents принимает словарь с годами и их количеством и находит год с наибольшим количеством событий.
+// Она возвращает словарь с одним элементом, где ключ - это год с наибольшим количеством событий, а значение - это это количество.
+Dictionary<string, int> FindYearWithMostEvents(Dictionary<string, int> yearOccurrences)
+{
+    var yearWithMostEvents = string.Empty;
+    var maxOccurrences = 0;
+
+    foreach (var pair in yearOccurrences)
+    {
+        if (pair.Value > maxOccurrences)
+        {
+            maxOccurrences = pair.Value;
+            yearWithMostEvents = pair.Key;
+        }
+    }
+
+    var result = new Dictionary<string, int>();
+    result.Add(yearWithMostEvents, maxOccurrences);
+
+    return result;
+}
+
+// Функция SerializeObject принимает объект и сериализует его в массив байтов с использованием JSON-сериализации.
+byte[] SerializeObject<T>(T obj)
+{
+    return JsonSerializer.SerializeToUtf8Bytes(obj);
+}
+
+// Функция DeserializeObject принимает массив байтов и десериализует его в объект с использованием JSON-десериализации.
+T? DeserializeObject<T>(byte[] arrBytes)
+{
+    return JsonSerializer.Deserialize<T>(arrBytes);
+}
---
+++ Laboratory4/ProgramB/Program.cs
@@ -0,0 +1,365 @@
+﻿using System.Diagnostics;
+using System.Text.Json;
+using MPI;
+using Environment = MPI.Environment;
+
+try
+{
+    // Создание MPI окружения и инициализация среды выполнения.
+    using (new Environment(ref args))
+    {
+        // Получение глобального коммуникатора MPI.
+        Intracommunicator world = Communicator.world;
+        int numProcesses = world.Size; // Общее количество процессов.
+        int processRank = world.Rank; // Ранг текущего процесса.
+
+        Stopwatch stopwatch = new Stopwatch(); // Инициализация счетчика времени.
+
+        // Загрузка данных выполняется только в мастер-процессе (ранг 0).
+        List<string?> tweets = new List<string?>();
+        if (processRank == 0)
+        {
+            stopwatch.Start();
+            string filePath = "FIFA.csv"; // Путь к файлу с данными. Укажите свой путь.
+            tweets = LoadData(filePath);
+        }
+
+        // Распространение данных всем процессам.
+        world.Broadcast(ref tweets, 0);
+
+        // Определение количества потоков для каждого процесса (2, 4, 6 или 8).
+        int numThreads = 2 * (processRank + 1);
+
+        // Разделение данных на части для каждого процесса.
+        List<List<string?>> dataChunks = PartitionData(tweets, numProcesses);
+
+        // Создание списков для хранения результатов обработки данных.
+        List<string> resultA = new List<string>();
+        List<string> resultB = new List<string>();
+        List<string> resultMatchingCapitals = new List<string>();
+
+        // Получение списка 10 наиболее упоминаемых хештегов (список А).
+        List<string> topHashtags = GetTopHashtags(tweets);
+
+        // Получение списка 10 столиц, из которых твиты отправлялись наиболее часто (список B).
+        List<string> topCapitals = GetTopCapitalCities(tweets);
+
+        // Параллельная обработка данных внутри каждого процесса с использованием нескольких потоков.
+        Parallel.ForEach(dataChunks[processRank], new ParallelOptions { MaxDegreeOfParallelism = numThreads }, tweet =>
+        {
+            // Обработка каждого твита и обновление результатов (A, B и matchingCapitals).
+            // Вызов соответствующих функций для обновления результатов.
+
+            // Пример обновления результата A (наиболее упоминаемые хештеги).
+            List<string> hashtagsInTweet = ExtractHashtags(tweet);
+            lock (resultA) // Блокировка для многопоточной безопасности.
+            {
+                foreach (string hashtag in hashtagsInTweet)
+                {
+                    if (topHashtags.Contains(hashtag))
+                    {
+                        resultA.Add(hashtag);
+                    }
+                }
+            }
+
+            // Пример обновления результата B (столицы, из которых твиты отправлялись наиболее часто).
+            string capitalOfTweet = ExtractCapital(tweet);
+            if (!string.IsNullOrEmpty(capitalOfTweet))
+            {
+                lock (resultB) // Блокировка для многопоточной безопасности.
+                {
+                    if (topCapitals.Contains(capitalOfTweet))
+                    {
+                        resultB.Add(capitalOfTweet);
+                    }
+                }
+            }
+
+            // Пример обновления результата matchingCapitals (столицы, в которых используются хештеги из списка topHashtags).
+            lock (resultMatchingCapitals) // Блокировка для многопоточной безопасности.
+            {
+                foreach (string hashtag in hashtagsInTweet)
+                {
+                    if (topHashtags.Contains(hashtag) && !string.IsNullOrEmpty(capitalOfTweet))
+                    {
+                        string capitalHashtagPair = $"{capitalOfTweet}|{hashtag}";
+                        resultMatchingCapitals.Add(capitalHashtagPair);
+                    }
+                }
+            }
+        });
+
+        // Сериализация результатов в JSON.
+        byte[] resultABytes = SerializeObject(resultA);
+        byte[] resultBBytes = SerializeObject(resultB);
+        byte[] resultMatchingCapitalsBytes = SerializeObject(resultMatchingCapitals);
+
+        // Мастер-процесс (ранг 0) собирает сериализованные результаты от всех процессов.
+        byte[][] allResultABytes = world.Gather(resultABytes, 0);
+        byte[][] allResultBBytes = world.Gather(resultBBytes, 0);
+        byte[][] allResultMatchingCapitalsBytes = world.Gather(resultMatchingCapitalsBytes, 0);
+
+        // Десериализация результатов.
+        List<string> finalResultA = new List<string>();
+        List<string> finalResultB = new List<string>();
+        List<string> finalResultMatchingCapitals = new List<string>();
+
+        if (processRank == 0)
+        {
+            foreach (var bytes in allResultABytes)
+            {
+                finalResultA.AddRange(DeserializeObject<List<string>>(bytes));
+            }
+
+            foreach (var bytes in allResultBBytes)
+            {
+                finalResultB.AddRange(DeserializeObject<List<string>>(bytes));
+            }
+
+            foreach (var bytes in allResultMatchingCapitalsBytes)
+            {
+                finalResultMatchingCapitals.AddRange(DeserializeObject<List<string>>(bytes));
+            }
+
+            // Завершающая обработка результатов и вывод.
+            Console.WriteLine("Список А (10 наиболее упоминаемых хештегов):");
+            foreach (string hashtag in finalResultA)
+            {
+                Console.WriteLine(hashtag);
+            }
+
+            Console.WriteLine("\nСписок B (10 столиц, из которых твиты отправлялись наиболее часто):");
+            foreach (string capital in finalResultB)
+            {
+                Console.WriteLine(capital);
+            }
+
+            Console.WriteLine("\nСтолицы из списка B, в которых самый часто используемый хештег принадлежит списку A:");
+            foreach (string matchingCapital in finalResultMatchingCapitals)
+            {
+                Console.WriteLine(matchingCapital);
+            }
+
+            stopwatch.Stop();
+
+            TimeSpan elapsed = stopwatch.Elapsed;
+            Console.WriteLine($"Время выполнения программы: {elapsed.TotalMilliseconds} миллисекунд");
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка при выполнении программы: {ex.Message}");
+}
+
+// Метод для загрузки данных из текстового файла.
+// Принимает путь к файлу 'filePath' и возвращает список строк (твитов).
+List<string> LoadData(string filePath)
+{
+    List<string> tweets = new List<string>(); // Создаем список для хранения твитов.
+
+    try
+    {
+        // Используем 'using' для гарантированного освобождения ресурсов после чтения файла.
+        using (var reader = new StreamReader(filePath))
+        {
+            while (!reader.EndOfStream) // Пока не достигнут конец файла.
+            {
+                string line = reader.ReadLine(); // Считываем строку из файла.
+                tweets.Add(line); // Добавляем считанную строку (твит) в список 'tweets'.
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        // Если возникает ошибка при чтении файла, выводим сообщение об ошибке.
+        Console.WriteLine($"Ошибка при загрузке данных: {ex.Message}");
+    }
+
+    return tweets; // Возвращаем список, содержащий загруженные твиты.
+}
+
+// Метод для извлечения хештегов из текста твита.
+// Принимает текст твита 'tweet' и возвращает список хештегов.
+List<string> ExtractHashtags(string tweet)
+{
+    List<string> hashtags = new List<string>(); // Создаем список для хранения хештегов.
+
+    string[] parts = tweet.Split(','); // Разбиваем текст твита на части по запятой.
+
+    if (parts.Length >= 10) // Проверяем, что в данных присутствует столбец с хештегами (нумерация с 0).
+    {
+        string hashtagsString = parts[9]; // Получаем строку с хештегами. Здесь указывается номер столбца.
+
+        if (!string.IsNullOrEmpty(hashtagsString)) // Проверяем, что строка с хештегами не пустая.
+        {
+            string[] hashtagArray = hashtagsString.Split(','); // Разбиваем строку на отдельные хештеги.
+
+            foreach (string hashtag in hashtagArray)
+            {
+                string
+                    cleanedHashtag =
+                        hashtag.Trim().ToLower(); // Очищаем хештег от символа '#' и приводим к нижнему регистру.
+
+                if (!string.IsNullOrEmpty(cleanedHashtag)) // Проверяем, что хештег не пустой после очистки.
+                {
+                    hashtags.Add(cleanedHashtag); // Добавляем очищенный хештег в список 'hashtags'.
+                }
+            }
+        }
+    }
+
+    return hashtags; // Возвращаем список извлеченных хештегов из твита.
+}
+
+// Метод для извлечения столицы из текста твита.
+// Принимает текст твита 'tweet' и возвращает строку с названием столицы.
+string ExtractCapital(string tweet)
+{
+    string capital = null; // Инициализируем переменную 'capital' как null.
+
+    string[] parts = tweet.Split(','); // Разбиваем текст твита на части по запятой.
+
+    if (parts.Length >= 14) // Проверяем, что в данных присутствует столбец с данными о столице (нумерация с 0).
+    {
+        capital = parts[13]; // Получаем строку с названием столицы. Здесь указывается номер столбца.
+
+        if (!string.IsNullOrEmpty(capital)) // Проверяем, что название столицы не пустое.
+        {
+            capital = capital.Trim(); // Удаляем лишние пробелы из названия столицы.
+        }
+    }
+
+    return capital; // Возвращаем название столицы или null, если не найдено.
+}
+
+// Метод для разделения данных на части (партиции).
+// Принимает список данных 'data' и количество партиций 'numPartitions'.
+// Возвращает список списков, где каждый список представляет собой партицию данных.
+List<List<string?>> PartitionData(List<string?> data, int numPartitions)
+{
+    List<List<string?>> partitions = new List<List<string?>>(numPartitions); // Создаем список для хранения партиций.
+
+    // Вычисляем размер каждой части данных (партиции).
+    int partitionSize = data.Count / numPartitions;
+
+    // Разделяем данные на части (партиции) в соответствии с заданным количеством партиций.
+    for (int i = 0; i < numPartitions; i++)
+    {
+        int startIndex = i * partitionSize; // Вычисляем начальный индекс для текущей партиции.
+        int endIndex = (i == numPartitions - 1) ? data.Count : startIndex + partitionSize; // Вычисляем конечный индекс.
+
+        // Получаем данные для текущей партиции и добавляем их в список 'partitions'.
+        List<string?> partition = data.GetRange(startIndex, endIndex - startIndex);
+        partitions.Add(partition);
+    }
+
+    return partitions; // Возвращаем список партиций данных.
+}
+
+// Метод для получения списка 10 наиболее упоминаемых хештегов из списка твитов.
+// Принимает список 'tweets', где каждый элемент представляет собой текст твита.
+// Возвращает список строк с хештегами.
+List<string> GetTopHashtags(List<string> tweets)
+{
+    Dictionary<string, int>
+        hashtagFrequency = new Dictionary<string, int>(); // Создаем словарь для подсчета частоты упоминания хештегов.
+
+    foreach (string tweet in tweets) // Проходим по каждому твиту в списке.
+    {
+        string[] parts = tweet.Split(','); // Разбиваем текст твита на части, предполагая разделение запятой.
+
+        if (parts.Length >= 10) // Убедитесь, что в данных присутствует столбец с хештегами (нумерация с 0).
+        {
+            string hashtags = parts[9]; // Получаем строку с хештегами. Здесь указывается номер столбца.
+
+            if (!string.IsNullOrEmpty(hashtags)) // Проверяем, что строка с хештегами не пустая.
+            {
+                string[] hashtagArray = hashtags.Split(','); // Разделяем хештеги по запятой.
+
+                foreach (string hashtag in hashtagArray) // Проходим по каждому хештегу.
+                {
+                    string cleanedHashtag =
+                        hashtag.Trim().ToLower(); // Удаляем знаки '#' и приводим хештег к нижнему регистру.
+
+                    if (!string.IsNullOrEmpty(cleanedHashtag)) // Проверяем, что хештег не пустой после очистки.
+                    {
+                        if (hashtagFrequency.ContainsKey(cleanedHashtag)) // Проверяем, есть ли хештег в словаре.
+                        {
+                            hashtagFrequency[cleanedHashtag]++; // Увеличиваем счетчик упоминаний хештега.
+                        }
+                        else
+                        {
+                            hashtagFrequency[cleanedHashtag] = 1; // Добавляем хештег в словарь, если его нет.
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Выбираем 10 наиболее упоминаемых хештегов, сортируя по частоте упоминаний.
+    List<string> topHashtags = hashtagFrequency.OrderByDescending(pair => pair.Value)
+        .Take(10)
+        .Select(pair => pair.Key)
+        .ToList();
+
+    return topHashtags; // Возвращаем список 10 наиболее упоминаемых хештегов.
+}
+
+// Метод для получения списка 10 наиболее упоминаемых столиц, из которых твиты отправлялись наиболее часто.
+// Принимает список 'tweets', где каждый элемент представляет собой текст твита.
+// Возвращает список строк с названиями столиц.
+List<string> GetTopCapitalCities(List<string?> tweets)
+{
+    Dictionary<string, int>
+        capitalFrequency = new Dictionary<string, int>(); // Создаем словарь для подсчета частоты упоминания столиц.
+
+    foreach (string? tweet in tweets) // Проходим по каждому твиту в списке.
+    {
+        string[] parts = tweet.Split(','); // Разбиваем текст твита на части, предполагая разделение запятой.
+
+        if (parts.Length >= 14) // Убедитесь, что в данных присутствует столбец с данными о столице (нумерация с 0).
+        {
+            string capital = parts[13]; // Получаем строку с названием столицы. Здесь указывается номер столбца.
+
+            if (!string.IsNullOrEmpty(capital)) // Проверяем, что строка с названием столицы не пустая.
+            {
+                if (capitalFrequency.ContainsKey(capital)) // Проверяем, есть ли столица в словаре.
+                {
+                    capitalFrequency[capital]++; // Увеличиваем счетчик упоминаний столицы.
+                }
+                else
+                {
+                    capitalFrequency[capital] = 1; // Добавляем столицу в словарь, если ее нет.
+                }
+            }
+        }
+    }
+
+    // Выбираем 10 наиболее упоминаемых столиц, сортируя по частоте упоминаний.
+    List<string> topCapitals = capitalFrequency.OrderByDescending(pair => pair.Value)
+        .Take(10)
+        .Select(pair => pair.Key)
+        .ToList();
+
+    return topCapitals; // Возвращаем список 10 наиболее упоминаемых столиц.
+}
+
+// Метод для сериализации объекта 'obj' в массив байтов.
+// Принимает объект 'obj' и возвращает массив байтов.
+static byte[] SerializeObject<T>(T obj)
+{
+    return
+        JsonSerializer.SerializeToUtf8Bytes(obj); // Используем JsonSerializer для сериализации объекта в массив байтов.
+}
+
+// Метод для десериализации массива байтов 'arrBytes' в объект типа 'T'.
+// Принимает массив байтов 'arrBytes' и возвращает десериализованный объект типа 'T'.
+static T DeserializeObject<T>(byte[] arrBytes)
+{
+    return
+        JsonSerializer
+            .Deserialize<T>(arrBytes); // Используем JsonSerializer для десериализации массива байтов в объект.
+}
